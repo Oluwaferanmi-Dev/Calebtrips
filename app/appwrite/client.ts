146e37567c9973010eeb072dfac9b20e181508d1
Caleb <92544582+Oluwaferanmi-Dev@users.noreply.github.com>
2025-08-05 00:26:24 +0100

    bug fixes

--- app/appwrite/client.ts
+++ app/appwrite/client.ts
@@ -1,12 +1,16 @@
 import {Account, Client, Databases, Storage} from "appwrite";
 
 export const appwriteConfig = {
-    endpointUrl: import.meta.env.VITE_APPWRITE_API_ENDPOINT,
-    projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
-    apiKey: import.meta.env.VITE_APPWRITE_API_KEY,
-    databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
-    userCollectionId: import.meta.env.VITE_APPWRITE_USERS_COLLECTION_ID,
-    tripCollectionId: import.meta.env.VITE_APPWRITE_TRIPS_COLLECTION_ID,
+    endpointUrl: process.env.VITE_APPWRITE_API_ENDPOINT, // Change here
+    projectId: process.env.VITE_APPWRITE_PROJECT_ID, // Change here
+    apiKey: process.env.VITE_APPWRITE_API_KEY, // Change here
+    databaseId: process.env.VITE_APPWRITE_DATABASE_ID, // Change here
+    userCollectionId: process.env.VITE_APPWRITE_USERS_COLLECTION_ID, // Change here
+    tripCollectionId: process.env.VITE_APPWRITE_TRIPS_COLLECTION_ID, // Change here
+}
+
+if (!appwriteConfig.endpointUrl || !appwriteConfig.projectId) {
+    throw new Error("Appwrite endpoint URL and project ID must be defined in environment variables.");
 }
 
 const client = new Client()
